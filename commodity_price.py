3fbeb38dde482122afd2596477a79ba5f6e2889e
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2024-04-07 02:18:31 +0530

    Unique district bug fixes

--- commodity_price.py
+++ commodity_price.py
@@ -10,6 +10,17 @@ list_of_states = df['State'].unique().tolist()
 selected_state = st.selectbox('Select a State', list_of_states)
 
 filtered_df = df[df['State'] == selected_state]
+state_district_map = {}
 
 
+for index, row in df.iterrows():
+    state = row['State']
+    district = row['District']
+    
+    
+    if state in state_district_map:
+        state_district_map[state].append(district)
+   
+    else:
+        state_district_map[state] = [district]
 st.write(filtered_df)
