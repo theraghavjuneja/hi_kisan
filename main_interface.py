fbc13ca1b867808c015c170bb5d07a91360eb178
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2024-04-06 19:56:20 +0530

    Configured different screens

--- main_interface.py
+++ main_interface.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pyttsx3
-from return_statements import heading_statements,image_uploader
+from return_statements import heading_statements, image_uploader
 
 def text_to_speech(text):
     engine = pyttsx3.init()
@@ -13,18 +13,23 @@ def main():
     st.title('HI-KISAN')
     st.sidebar.title('Language/भाषा')
     language = st.sidebar.selectbox("", ["English", "Hindi"])
-    heading_statement = heading_statements()
+    screens = st.selectbox("Choose screens", ["Main Screen","Crop Predictor", "Chat with AI", "Screen 3", "Screen 4", "Screen 5"])
+    if screens=="Main Screen":
+        if language=="English":
+            heading_statement = heading_statements()
+            for i, statement in enumerate(heading_statement):
+                col1, col2 = st.columns([3, 1]) 
+                with col1:
+                    st.write(statement)
+                with col2:
+                    st.button(f"Listen", key=f"speak_{i}", on_click=lambda text=statement: text_to_speech(text))     
+        
+    if screens=="Crop Predictor":
+        image_uploader()
 
-    if language == "English":
-        for i, statement in enumerate(heading_statement):
-            col1, col2 = st.columns([3, 1])  # Adjust the ratio as per your preference
-            with col1:
-                st.write(statement)
-            with col2:
-                st.button(f"Listen", key=f"speak_{i}", on_click=lambda text=statement: text_to_speech(text))
-        image_uploader()  
-    elif language == 'Hindi':
-        pass
+    
+          
+    
 
 if __name__ == "__main__":
     main()
