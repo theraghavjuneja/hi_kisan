aaa56064a3a0116c36604c2234d6cd45815931dc
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2024-04-07 02:22:56 +0530

    Configured files for entire df

--- commodity_price.py
+++ commodity_price.py
@@ -4,23 +4,25 @@ import streamlit as st
 # Load the data
 df = pd.read_csv('commodities_data.csv')
 
-
+# Get unique states
 list_of_states = df['State'].unique().tolist()
 
-selected_state = st.selectbox('Select a State', list_of_states)
-
-filtered_df = df[df['State'] == selected_state]
+# Create a dictionary to map states to their corresponding districts
 state_district_map = {}
+for state in list_of_states:
+    state_district_map[state] = df[df['State'] == state]['District'].unique().tolist()
 
+# Get user input for state selection
+selected_state = st.selectbox('Select a State', list_of_states)
+st.info("ALL THE PRICES ARE PER QUINTAL OF THE COMMODITY")
+# Display the corresponding districts for the selected state
+if selected_state in state_district_map:
+    selected_district = st.selectbox('Select a District', state_district_map[selected_state])
+else:
+    st.write("No districts available for the selected state.")
 
-for index, row in df.iterrows():
-    state = row['State']
-    district = row['District']
-    
-    
-    if state in state_district_map:
-        state_district_map[state].append(district)
-   
-    else:
-        state_district_map[state] = [district]
+# Filter data based on user selection
+filtered_df = df[(df['State'] == selected_state) & (df['District'] == selected_district)]
+
+# Display filtered data
 st.write(filtered_df)
